b62a5c98e28c4bdbf1d73b95257a4312f959ed91
yangliang <yangliang@szca.com.cn>
2019-10-11 15:28:00 +0800

    配置文件，数据库模型类创建

--- sayhello/models.py
+++ sayhello/models.py
@@ -4,3 +4,12 @@
     :copyright: © 2019 Sam Yang 
     :license: MIT, see LICENSE for more details.
 """
+from datetime import  datetime
+
+from sayhello import  db
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    body = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
--- sayhello/settings.py
+++ sayhello/settings.py
@@ -4,3 +4,20 @@
     :copyright: © 2019 Sam Yang 
     :license: MIT, see LICENSE for more details.
 """
+
+import os
+import sys
+
+from sayhello import  app
+
+WIN = sys.platform.startswith('win')
+
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
