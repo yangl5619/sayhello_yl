6ed4906dee02d4a750bf48fb6aef6217236a61e3
yangliang <yangliang@szca.com.cn>
2019-10-12 10:54:23 +0800

    index页面，views视图

--- sayhello/forms.py
+++ sayhello/forms.py
@@ -4,3 +4,12 @@
     :copyright: © 2019 Sam Yang 
     :license: MIT, see LICENSE for more details.
 """
+
+from  flask_wtf import  FlaskForm
+from  wtforms import  StringField, SubmitField, TextAreaField
+from  wtforms.validators import  DataRequired, Length
+
+class HelloForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
+    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
+    submit = SubmitField()
--- sayhello/views.py
+++ sayhello/views.py
@@ -4,3 +4,22 @@
     :copyright: © 2019 Sam Yang 
     :license: MIT, see LICENSE for more details.
 """
+from  flask import  flash, redirect, url_for, render_template
+
+from sayhello import  app, db
+from sayhello.forms import HelloForm
+from sayhello.models import Message
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
+    form = HelloForm()
+    if form.validate_on_submit():
+        name = form.name
+        body = form.body
+        message = Message(name=name, body=body)
+        db.session.add(message)
+        db.session.commit()
+        flash('Your message have been sent to you world!')
+        return redirect('index.html')
+    return render_template('index.html', form=form, messages=messages)
